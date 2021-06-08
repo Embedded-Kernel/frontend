4dd590254bdebd8ec272a68b6c13f547ab83e966
Denyse <73523419+dmutoni@users.noreply.github.com>
2021-06-08 16:22:22 +0200

    login page done

--- components/HomeSideBar.tsx
+++
@@ -1,8 +0,0 @@
-const HomeSidebar = (props: Record<string,string>) => {
-    return (
-        <div className="col-span-3 bg-sideBarColor h-screen">
-            <p className="text-white font-[semi-bold] mt-32 text-center text-5xl">Smart SC</p>
-        </div>
-    )
-}
-export default HomeSidebar;
