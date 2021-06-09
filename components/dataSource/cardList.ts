f32dde17c7b5a8c02c08d7d6531328f03df4a1e1
chanelle740 <hirwa.chanelle@gmail.com>
2021-06-09 12:10:23 +0200

    feat:some changes

--- components/dataSource/cardList.ts
+++ components/dataSource/cardList.ts
@@ -55,5 +55,19 @@ export const peoples = [
         year: "Year 1",
         class: "class B",
       },
+      {
+        name: "Makuza Mugabo",
+        cardno: "RCA0114MHB",
+        balance: "20000RWF",
+        year: "Year 1",
+        class: "class B",
+      },
+      {
+        name: "Makuza Mugabo",
+        cardno: "RCA0114MHB",
+        balance: "20000RWF",
+        year: "Year 1",
+        class: "class B",
+      },
       
     ];
