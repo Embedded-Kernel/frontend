68fef4501ce660a434cfe982a4604f712d421f7c
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-06-14 12:19:47 +0200

    (chore): fix deployment and remove un ncessary packages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,13 @@
-import Head from "next/head";
 import { Fragment } from "react";
 import StudentCardList from "../components/StudentCardList";
-import SideBar from "../components/SideBar";
-import accountInfoPage from "./accountInfoPage";
+
 export default function Home() {
-    return (
-        <Fragment>
-            <main>
-               
-            {/* <SideBar /> */}
-            <StudentCardList /> 
-            </main>
-        </Fragment>
-    );
+  return (
+    <Fragment>
+      <main>
+        {/* <SideBar /> */}
+        <StudentCardList />
+      </main>
+    </Fragment>
+  );
 }
