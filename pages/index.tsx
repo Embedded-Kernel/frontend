ce157e9ffed82144f069f0be957d95d6180bbbbe
chanelle740 <hirwa.chanelle@gmail.com>
2021-08-27 11:39:24 +0200

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,28 @@
-import { Fragment } from "react";
-import { Route, StaticRouter as Router, Switch } from "react-router-dom";
+import React, { Fragment } from "react";
+import { StaticRouter as Router, Route,  Switch } from 'react-router-dom';
 import ConfirmCode from "../components/ConfirmationCode";
 import StudentCardList from "../components/StudentCardList";
 import {TransactionsList} from "../components/TransactionsList";
 import SignUpPage from "./register";
+
 
 export default function Home() {
   return (
 
     <Fragment>
       <main>
-        <ConfirmCode/>
-{/* <TransactionsList/> */}
-        {/* <StudentCardList/> */}
-      {/* <Switch>
-          <Route exact path="/" component={SignUpPage} />
-          <Route exact path="cards" component={StudentCardList} />
-          <Route exact path="/transactions" component={TransactionsList} />
-        </Switch> */}
+        <Router>
+      <Switch>
+              <Route exact path='/' component={SignUpPage}></Route>
+              <Route exact path='/transaction' component={TransactionsList}></Route>
+              <Route exact path='/cards' component={StudentCardList}></Route>
+              <Route exact path='/code' component={ConfirmCode}></Route>
+            </Switch>
+
+</Router>
+
+    
+
       </main>
     </Fragment>
  
