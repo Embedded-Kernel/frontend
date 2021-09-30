9af0f6fc814cdd64a2ef8a8d450301869f691ead
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-09-30 22:22:22 +0200

    feat: setting up home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,53 @@
-import React, { Fragment } from "react";
-import { StaticRouter as Router, Route,  Switch } from 'react-router-dom';
-import ConfirmCode from "../components/ConfirmationCode";
-import StudentCardList from "../components/StudentCardList";
-import {TransactionsList} from "../components/TransactionsList";
-import SignUpPage from "./register";
-
+import React from 'react';
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
   return (
+    <>
+      <Head>
+        <title>Embeded Kernel</title>
+      </Head>
 
-    <Fragment>
-      <main>
-        <Router>
-      <Switch>
-              <Route exact path='/' component={SignUpPage}></Route>
-              <Route exact path='/transaction' component={TransactionsList}></Route>
-              <Route exact path='/cards' component={StudentCardList}></Route>
-              <Route exact path='/code' component={ConfirmCode}></Route>
-            </Switch>
+      <main className="font-poppins">
+        <nav className="flex justify-around py-4">
+          <Image src="/Scs.png" width={100} height={30} alt="logo image" />
 
-</Router>
-
-    
-
+          <ul className="flex space-x-4 font-semibold items-center text-secondary">
+            <li>
+              <Link href="/about">
+                <a>About us</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="partiners">
+                <a>Our partners</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="why">
+                <a>Why us</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="contact">
+                <a>Contact us</a>
+              </Link>
+            </li>
+            <li className="bg-primary px-8 py-2 rounded-md">
+              <Link href="login">
+                <a>Login</a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <div className="flex justify-center items-center min-h-screen">
+          <h1 className="font-bold text-8xl">
+            Smart <span className="text-secondary">student</span> card
+          </h1>
+        </div>
       </main>
-    </Fragment>
- 
+    </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,25 @@
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     //
     extend: {
       backgroundColor: {
-        sideBarColor : '#2D2D2F'
+        primary: '#2F2E41',
+        secondary: '#FFC700',
+        sideBarColor: '#2D2D2F',
       },
       fontFamily: {
-        'sans': ['Roboto', 'Helvetica', 'Arial', 'sans-serif']
-      }
+        sans: ['Roboto', 'Helvetica', 'Arial', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      textColor: {
+        secondary: '#FFC700',
+      },
     },
   },
   variants: {
-    extend: {
-
-    },
+    extend: {},
   },
   plugins: [],
 };
