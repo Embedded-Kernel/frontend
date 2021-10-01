db252e5f8cba4592e02629420853c61e023b6056
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-10-01 22:18:08 +0200

    feat: fixed few stuffs

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,39 +10,43 @@ export default function Home() {
         <title>Embeded Kernel</title>
       </Head>
 
-      <main className="font-poppins">
-        <nav className="flex justify-around py-4">
-          <Image src="/Scs.png" width={100} height={30} alt="logo image" />
+      <main className="font-poppins min-h-screen">
+          <nav className="flex justify-around block py-4">
 
-          <ul className="flex space-x-4 font-semibold items-center text-secondary">
-            <li>
-              <Link href="/about">
-                <a>About us</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="partiners">
-                <a>Our partners</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="why">
-                <a>Why us</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="contact">
-                <a>Contact us</a>
-              </Link>
-            </li>
-            <li className="bg-primary px-8 py-2 rounded-md">
-              <Link href="login">
-                <a>Login</a>
-              </Link>
-            </li>
-          </ul>
-        </nav>
-        <div className="flex justify-center items-center min-h-screen">
+    <div>
+
+    </div>
+
+            <ul className="flex items-center font-semibold space-x-4 text-secondary">
+              <li>
+                <Link href="/about">
+                  <a>About us</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="partiners">
+                  <a>Our partners</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="why">
+                  <a>Why us</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="contact">
+                  <a>Contact us</a>
+                </Link>
+
+              </li>
+              <li className="px-8 py-2 bg-primary rounded-md">
+                <Link href="login">
+                  <a>Login</a>
+                </Link>
+                </li>
+            </ul>
+          </nav>
+        <div className="flex items-center justify-center h-full">
           <h1 className="font-bold text-8xl">
             Smart <span className="text-secondary">student</span> card
           </h1>
