db167f70a2b046bd475943afbd3cd88d804aec2b
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-10-02 07:16:51 +0200

    chore: prepare something

---
+++ components/Hero.tsx
@@ -0,0 +1,9 @@
+export function Hero() {
+  return (
+    <main className="flex justify-center items-center " style={{ height: '80vh' }}>
+      <h1 className="font-bold text-8xl">
+        Smart <span className="text-secondary">student</span> card
+      </h1>
+    </main>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Head from 'next/head';
-import Image from 'next/image';
-import Link from 'next/link';
+import { Nav } from '../components/Nav';
+import { Hero } from '../components/Hero';
 
 export default function Home() {
   return (
@@ -10,48 +10,8 @@ export default function Home() {
         <title>Embeded Kernel</title>
       </Head>
 
-      <main className="font-poppins min-h-screen">
-          <nav className="flex justify-around block py-4">
-
-    <div>
-
-    </div>
-
-            <ul className="flex items-center font-semibold space-x-4 text-secondary">
-              <li>
-                <Link href="/about">
-                  <a>About us</a>
-                </Link>
-              </li>
-              <li>
-                <Link href="partiners">
-                  <a>Our partners</a>
-                </Link>
-              </li>
-              <li>
-                <Link href="why">
-                  <a>Why us</a>
-                </Link>
-              </li>
-              <li>
-                <Link href="contact">
-                  <a>Contact us</a>
-                </Link>
-
-              </li>
-              <li className="px-8 py-2 bg-primary rounded-md">
-                <Link href="login">
-                  <a>Login</a>
-                </Link>
-                </li>
-            </ul>
-          </nav>
-        <div className="flex items-center justify-center h-full">
-          <h1 className="font-bold text-8xl">
-            Smart <span className="text-secondary">student</span> card
-          </h1>
-        </div>
-      </main>
+      <Nav />
+      <Hero />
     </>
   );
 }
