49c17158a0ca5966482692182c782a2c7077bc20
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-10-08 07:16:13 +0200

    fix: update

---
+++ pages/app.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function App() {
+  return (
+    <>
+      <Head>
+        <title>application</title>
+      </Head>
+      <div>
+        <h1>Dashboard will go here</h1>
+      </div>
+    </>
+  );
+}
